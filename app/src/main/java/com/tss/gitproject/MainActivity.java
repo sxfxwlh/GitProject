423ef4edbfefaa8a2b3c7ef8b07c7d300d4668a0
hubert <sxfxwlh@126.com>
2017-05-09 10:14:55 +0800

    user2 change.

--- app/src/main/java/com/tss/gitproject/MainActivity.java
+++ app/src/main/java/com/tss/gitproject/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e(TAG,"this is user2 edit");
-        Log.e(TAG,"this is user1 edit");
+        Log.e(TAG,"this is user2 edit, too");
     }
 }
